d4052cc50f7f28a312b2ef6af832e6487c6e0082
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-21 11:51:14 +0200

    first commit

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song drink = new Song();
+            string displayBeerSong = drink.BeerSong();
+            Console.WriteLine(displayBeerSong);
+        }
+    }
+}
---
+++ Assignment 2/Song.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    public class Song
+    {
+        public string BeerSong()
+        {
+            StringBuilder lyrics = new StringBuilder();
+
+            //string lyrics = string.Empty;
+
+
+            for (int index = 99; index > 0; index--)
+            {
+                //if (index == 0)//ahead when I use the variables
+                // break; //determinated a block of code that is running
+
+                StringBuilder line = new StringBuilder(); //append is like +=
+
+                if (index != 1)
+                {
+                    if (index == 2)
+                    {
+                        line.AppendLine($"{index} bottles of beer on the wall, {index} bottles of beer.");
+                        line.AppendLine($"Take one down and pass it around, {index - 1} bottle of beer on the wall.");
+
+                    }
+                    else
+                    {
+                        line.AppendLine($"{index} bottles of beer on the wall, {index} bottles of beer.");
+                        line.AppendLine($"Take one down and pass it around, {index - 1} bottles of beer on the wall.");
+                    }
+
+                }
+                else
+                {
+                    line.AppendLine($"{index} bottle of beer on the wall, {index} bottle of beer.");
+                    line.AppendLine($"Take one down and pass it around, {index - 1} bottles of beer on the wall.");
+                }
+                lyrics.AppendLine(line.ToString());
+                //lyrics += line + "\n";
+            }
+            return lyrics.ToString();
+        }
+    }
+}
